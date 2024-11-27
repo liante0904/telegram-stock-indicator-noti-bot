09644299d0572f3a6aa475b6b619cf90c07bb407
seunghoon shin <liante0904@gmail.com>
2024-11-27 17:17:43 +0900

    add create_pdf

--- modules/nasdaq_52wk.py
+++ modules/nasdaq_52wk.py
@@ -7,6 +7,9 @@ import asyncio
 from tqdm import tqdm
 import os
 from dotenv import load_dotenv
+# import pandas as pd
+# from fpdf import FPDF
+
 
 
 # 현재 파일 기준으로 상위 디렉토리에 있는 .env 파일 경로 설정
@@ -148,6 +151,43 @@ async def analyze_nasdaq100():
 
     return high_52_week_stocks_list, str_msg
 
+# # PDF 생성
+# create_pdf('stocks_info.pdf', grouped_by_sector)
+
+# PDF 작성 함수
+# def create_pdf(filename, data):
+#     pdf = FPDF()
+#     pdf.set_auto_page_break(auto=True, margin=5)
+#     pdf.add_page()
+
+#     # 나눔고딕 폰트 추가 (정상 폰트와 굵은 폰트 둘 다 등록)
+#     pdf.add_font('NanumGothic', '', '/usr/share/fonts/truetype/nanum/NanumGothic.ttf', uni=True)
+#     pdf.add_font('NanumGothic-Bold', '', '/usr/share/fonts/truetype/nanum/NanumGothicBold.ttf', uni=True)
+
+#     # 기본 폰트 설정
+#     # pdf.set_font('NanumGothic', size=16)
+
+#     # 데이터가 그룹화된 상태일 경우
+#     for sector, group in data:
+#         pdf.set_text_color(0, 0, 0)
+        
+#         # 각 섹터 제목을 굵은 폰트로 출력
+#         pdf.set_font('NanumGothic-Bold', size=18)
+#         pdf.cell(200, 10, txt=f"섹터: {sector}", ln=True)
+
+#         # 각 그룹 내의 항목들을 출력
+#         for _, item in group.iterrows():
+#             # 티커를 굵은 폰트로 출력
+#             pdf.set_font('NanumGothic-Bold', size=18)
+#             pdf.cell(200, 10, txt=f"티커: {item['Ticker']}", ln=True)
+
+#             # 사업 내용을 기본 스타일로 출력
+#             pdf.set_font('NanumGothic', size=16)
+#             pdf.multi_cell(0, 10, txt=f"사업내용: {item['Business Profile']}", align="L")
+#             pdf.ln(5)  # 줄 간격 추가
+
+#     pdf.output(filename)
+    
 if __name__ == '__main__':
     # 메인 함수 실행
     asyncio.run(analyze_nasdaq100())
